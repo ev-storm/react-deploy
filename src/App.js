a1f0765fc5e44f6c738529c9330bb01d927689b8
ev-storm <ev.git@ouylook.com>
2025-01-07 19:36:01 +0300

    v2

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ class App extends Component {
     return (
       <div className="container">
         <div className="deploi-img"></div>
-        <h1>Деплой!!</h1>;
+        <h1>Деплой!!</h1>;<h1>v2.0</h1>;
       </div>
     );
   }
