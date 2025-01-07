0e945d9b43fabbdfaea8290617a34f43d8157b86
ev-storm <ev.git@ouylook.com>
2025-01-07 17:15:36 +0300

    11

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import "./index.css";
+import React, { Component } from "react";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="container">
+        <div className="deploi-img"></div>
+        <h1>Деплой!!</h1>;
+      </div>
+    );
+  }
+}
+
+export default App;
